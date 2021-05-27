5ce5dce4b8049870e67f738c300cbd8e04fd0564
dankelleher <dankelleher@yahoo.com>
2021-05-27 11:00:45 +0200

    v0.1.8

---
+++ client/src/api/addToChannel.ts
@@ -0,0 +1,82 @@
+import {
+  currentCluster,
+  makeKeypair,
+  PublicKeyBase58,
+  ReadRequest,
+} from '../lib/util';
+import * as service from '../service/get';
+import { SolanaUtil } from '../lib/solana/solanaUtil';
+import {
+  DecentralizedIdentifier,
+  keyToIdentifier,
+} from '@identity.com/sol-did-client';
+import { distinct, switchMap } from 'rxjs/operators';
+import { from, Observable } from 'rxjs';
+import { PublicKey } from '@solana/web3.js';
+import { Channel } from '../lib/Channel';
+
+type Message = {
+  sender: PublicKeyBase58;
+  content: string;
+};
+
+const didFromKey = (request: ReadRequest): Promise<string> => {
+  if (request.memberDID) return Promise.resolve(request.memberDID);
+  if (request.member)
+    return keyToIdentifier(
+      new PublicKey(request.member),
+      currentCluster(request.cluster)
+    );
+  return keyToIdentifier(
+    makeKeypair(request.decryptionKey).publicKey,
+    currentCluster(request.cluster)
+  );
+};
+
+/**
+ * Reads an channel
+ * @param request
+ */
+export const read = async (request: ReadRequest): Promise<Message[]> => {
+  const connection = SolanaUtil.getConnection(request.cluster);
+
+  const memberDID = await didFromKey(request);
+
+  const channel = await service.get(
+    new PublicKey(request.channel),
+    connection,
+    memberDID,
+    request.decryptionKey,
+    request.cluster
+  );
+
+  if (!channel) throw new Error('No channel found');
+
+  return channel.messages;
+};
+
+/**
+ * Subscribe to channel updates
+ * @param request
+ */
+export const readStream = (request: ReadRequest): Observable<Message> => {
+  const connection = SolanaUtil.getConnection(request.cluster);
+
+  const memberDID$ = from(didFromKey(request));
+
+  return memberDID$.pipe(
+    switchMap((memberDID: string) => {
+      const channel$ = service.getStream(
+        new PublicKey(request.channel),
+        connection,
+        memberDID,
+        request.decryptionKey,
+        request.cluster
+      );
+      const uniqueKey = (m: Message) => m.content + m.sender; // TODO add timestamp
+
+      return channel$
+        .pipe(switchMap((channel: Channel) => channel.messages))
+        .pipe(distinct(uniqueKey)); // only emit a message once
+    }));
+};
---
+++ client/src/service/addToChannel.ts
@@ -0,0 +1,159 @@
+import {Connection, Keypair, PublicKey} from '@solana/web3.js';
+import {
+  debug,
+  didToPublicKey,
+  ExtendedCluster,
+  isKeypair, isString,
+  pubkeyOf,
+} from '../lib/util';
+import { SolariumTransaction } from '../lib/solana/transaction';
+import { get as getDID } from '../lib/did/get';
+import { create as createDID } from '../lib/did/create';
+import { DIDDocument } from 'did-resolver';
+import { defaultSignCallback, SignCallback } from '../lib/wallet';
+import { SolanaUtil } from '../lib/solana/solanaUtil';
+import {createEncryptedCEK, encryptCEKForDID, generateCEK} from "../lib/crypto/ChannelCrypto";
+import {ChannelData} from "../lib/solana/models/ChannelData";
+import {resolve} from "@identity.com/sol-did-client";
+import {Channel} from "../lib/Channel";
+import {get} from "./get";
+
+/**
+ * If a DID was already registered for this owner, return its document. Else create one
+ * @param owner
+ * @param payer
+ * @param signCallback
+ * @param cluster
+ */
+const getOrCreateDID = async (
+  owner: PublicKey,
+  payer: Keypair | PublicKey,
+  signCallback: SignCallback,
+  cluster?: ExtendedCluster
+): Promise<DIDDocument> => {
+  try {
+    debug(`Looking for a DID owned by ${owner.toBase58()}`);
+    return await getDID(owner, cluster);
+  } catch (error) {
+    if (error.message.startsWith('No DID found')) {
+      debug('No DID found - creating...');
+
+      return createDID(owner, pubkeyOf(payer), signCallback, cluster);
+    }
+    throw error;
+  }
+};
+
+const getChannel = async (
+  owner: Keypair | PublicKey,
+  ownerDID: string,
+  channelAddress: PublicKey,
+  connection: Connection,
+  cluster?: ExtendedCluster
+): Promise<Channel> => {
+  const ownerKey = isKeypair(owner) ? owner.secretKey : undefined;
+  const channel = await get(channelAddress, connection, ownerDID, ownerKey, cluster);
+
+  if (!channel) {
+    throw new Error('Error retrieving created channel');
+  }
+
+  return channel;
+}
+
+/**
+ * Creates a group channel
+ * @param owner
+ * @param payer
+ * @param name
+ * @param signCallback
+ * @param cluster
+ */
+export const createChannel = async (
+  owner: Keypair | PublicKey,
+  payer: Keypair | PublicKey,
+  name: string,
+  signCallback?: SignCallback,
+  cluster?: ExtendedCluster
+): Promise<Channel> => {
+  const createSignedTx =
+    signCallback || (isKeypair(payer) && isKeypair(owner) && defaultSignCallback(payer, owner));
+  if (!createSignedTx) throw new Error('No payer or sign callback specified');
+
+  const ownerDIDDocument = await getOrCreateDID(
+    pubkeyOf(owner),
+    payer,
+    createSignedTx,
+    cluster
+  );
+  const didKey = didToPublicKey(ownerDIDDocument.id);
+  
+  const ceks = await createEncryptedCEK(ownerDIDDocument.id);
+  
+  const connection = SolanaUtil.getConnection(cluster);
+
+  const channelAddress = await SolariumTransaction.createGroupChannel(
+    connection,
+    pubkeyOf(payer),
+    didKey,
+    pubkeyOf(owner),
+    name,
+    ceks,
+    createSignedTx,
+    cluster
+  );
+  
+  return getChannel(owner, ownerDIDDocument.id, channelAddress, connection, cluster);
+};
+
+/**
+ * Creates a direct channel between two DIDs
+ * @param owner
+ * @param payer
+ * @param inviteeDID
+ * @param signCallback
+ * @param cluster
+ */
+export const createDirectChannel = async (
+  owner: Keypair | PublicKey,
+  payer: Keypair | PublicKey,
+  inviteeDID: string,
+  signCallback?: SignCallback,
+  cluster?: ExtendedCluster
+): Promise<Channel> => {
+  const createSignedTx =
+    signCallback || (isKeypair(payer) && isKeypair(owner) && defaultSignCallback(payer, owner));
+  if (!createSignedTx) throw new Error('No payer or sign callback specified');
+
+  // Create the owner DID if it doesn't exist
+  const ownerPubKey = pubkeyOf(owner);
+  const ownerDIDDocument = await getOrCreateDID(
+    ownerPubKey,
+    payer,
+    createSignedTx,
+    cluster
+  );
+
+  const inviteeDIDDocument = await resolve(inviteeDID)
+  
+  // create and encrypt a CEK for the new channel
+  const cek = await generateCEK();
+  const ownerCEKs = await encryptCEKForDID(cek, ownerDIDDocument.id);
+  const inviteeCEKs = await encryptCEKForDID(cek, inviteeDIDDocument.id);
+
+  const connection = SolanaUtil.getConnection(cluster);
+
+  // create the channel
+  const channelAddress = await SolariumTransaction.createDirectChannel(
+    pubkeyOf(payer),
+    didToPublicKey(ownerDIDDocument.id),
+    ownerPubKey,
+    didToPublicKey(inviteeDID),
+    ownerCEKs,
+    inviteeCEKs,
+    createSignedTx,
+    cluster
+  );
+
+  return getChannel(owner, ownerDIDDocument.id, channelAddress, connection, cluster);
+};

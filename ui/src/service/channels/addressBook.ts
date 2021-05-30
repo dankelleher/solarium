ea81f10ae142aae81567d4a265b59cba8d7aabe5
Martin Riedel <web@riedel-privat.de>
2021-05-29 20:30:24 -0400

    fix: Default to a single cluster name and endpoint.

--- ui/src/service/channels/addressBook.ts
+++ ui/src/service/channels/addressBook.ts
@@ -1,16 +1,18 @@
 import {ENDPOINTS} from "../constants";
 import {DEFAULT_ENDPOINT_INDEX} from "../web3/connection";
-import {addToChannel, cluster, getChannel, getDirectChannel} from "./solarium";
+import {addToChannel, getChannel, getDirectChannel} from "./solarium";
 import {Connection, Keypair} from "@solana/web3.js";
 import Wallet from "@project-serum/sol-wallet-adapter";
 import {Channel} from "solarium-js";
 import * as u8a from 'uint8arrays'
 
+const cluster = ENDPOINTS[DEFAULT_ENDPOINT_INDEX].name;
+
 type PublicChannelConfig = { [key: string]: GroupChannelConfig[] }
 export const publicChannelConfig: PublicChannelConfig = require('./publicChannels.json');
 
-export const publicChannelConfigByName = (name:string) => 
-  publicChannelConfig[cluster!]
+export const publicChannelConfigByName = (name:string) =>
+  publicChannelConfig[cluster]
     .find(config => config.name === name)
 
 type GroupChannelConfig ={
@@ -36,7 +38,7 @@ export type DirectChannel = {
   channel: Channel
 }
 
-export const isGroupChannel = 
+export const isGroupChannel =
   (groupOrDirectChannel: Channel | DirectChannel): groupOrDirectChannel is Channel =>
     Object.prototype.hasOwnProperty.call(groupOrDirectChannel, 'address')
 
@@ -51,7 +53,7 @@ export class AddressBookManager {
     readonly groupChannels: Channel[],
     readonly directChannels: DirectChannel[]) {
   }
-  
+
   getChannelByName(name: string): Channel | undefined {
     return this.groupChannels.find(c => c.name === name);
   }
@@ -63,7 +65,7 @@ export class AddressBookManager {
   getDirectChannelByContactAlias(alias: string): DirectChannel | undefined {
     return this.directChannels.find(c => c.contact.alias === alias);
   }
-  
+
   getGroupOrDirectChannelByAddress(address: string) : Channel | DirectChannel | undefined {
     const groupChannel = this.groupChannels.find(c => c.address.toBase58() === address);
     if (groupChannel) return groupChannel;
@@ -76,32 +78,31 @@ export class AddressBookManager {
     if (!channelConfig.inviteAuthority) {
       throw new Error("Cannot join " + channelConfig.name + " - no invite authority");
     }
-    
+
     const inviteAuthorityKeypair = Keypair.fromSecretKey(base58ToBytes(channelConfig.inviteAuthority));
 
     await addToChannel(this.connection, this.wallet, channelConfig.address, inviteAuthorityKeypair, this.did)
-    
+
     const channel = await getChannel(this.connection, this.wallet, this.did, channelConfig.address, this.decryptionKey);
-    
+
     if (!channel) throw new Error("Unable to retrieve channel " + channelConfig.name);
-    
+
     this.groupChannels.push(channel);
-    
+
     return channel;
   }
 
   static async load(store: AddressBookConfig, connection: Connection, wallet: Wallet, did: string, decryptionKey: Keypair): Promise<AddressBookManager> {
-    const currentCluster = ENDPOINTS[DEFAULT_ENDPOINT_INDEX].name;
 
     const mergedConfig: AddressBookConfig = {
-      channels: [...store.channels, ...publicChannelConfig[currentCluster as string]],
+      channels: [...store.channels, ...publicChannelConfig[cluster as string]],
       contacts: store.contacts,
     }
 
     const groupChannelPromises = mergedConfig.channels.map(c => getChannel(connection, wallet, did, c.address, decryptionKey));
 
     const groupChannelResults = await Promise.allSettled(groupChannelPromises)
-    
+
     const groupChannels = groupChannelResults
       .filter(result => result.status === 'fulfilled')
       // @ts-ignore
@@ -114,17 +115,17 @@ export class AddressBookManager {
         contact.did,
         decryptionKey
         )
-      
+
       if (!channel) throw new Error("No direct channel created for contact " + contact.did);
-      
+
       return {
         channel,
         contact
       }
     })
-    
+
     const directChannels = await Promise.all(directChannelsPromises);
-    
+
     return new AddressBookManager(
       connection,
       wallet,

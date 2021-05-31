9880af2667c5df2fcc4c135bb54be5a3c0b0e4eb
dankelleher <dankelleher@yahoo.com>
2021-05-31 12:36:26 +0200

    Turn useIdentity into a React Context. Hook up  joinPublicChannel to the onboarding wizard.

--- ui/src/components/onboarding/OnboardingModal.tsx
+++ ui/src/components/onboarding/OnboardingModal.tsx
@@ -12,6 +12,8 @@ type Props = {
 }
 export default ({ title, steps, currentStepIndex, next }:Props) => {
   const [open, setOpen] = useState(true)
+  
+  console.log("CURRENT STEP " + currentStepIndex);
 
   const nextRef = useRef(null)
 

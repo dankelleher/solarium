34d1616e4e1ea4b88031303d52345531eca9d91b
Martin Riedel <web@riedel-privat.de>
2021-05-31 19:16:10 -0400

    feat: Direct Channel (Contact) list component added.

--- ui/src/components/ChannelList.tsx
+++ ui/src/components/ChannelList.tsx
@@ -1,5 +1,4 @@
 import {useChannel} from "../service/channels/channel";
-import MessageView from "./MessageView";
 import {Channel} from "solarium-js";
 import {DirectChannel} from "../service/channels/addressBook";
 
@@ -31,19 +30,59 @@ const ChannelList = () => {
                                   </p>
                               </div>
                               <div>
-                                  <button
-                                      onClick={() => setCurrentChannel(ch)}
-                                      disabled={ channel && channel.address.toBase58() === ch.address.toBase58() }
-                                      className="inline-flex bg-myrtleGreen items-center px-2.5 py-1.5 border border-transparent text-xs font-medium rounded shadow-sm text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
-                                      Join
-                                  </button>
+                                  {channel?.address.toBase58() !== ch.address.toBase58() && <button
+                                    onClick={() => setCurrentChannel(ch)}
+                                    className="inline-flex bg-myrtleGreen items-center px-2.5 py-1.5 border border-transparent text-xs font-medium rounded shadow-sm text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+                                    Join
+                                  </button>}
+
                               </div>
                           </div>
                       </li>
                   )}
               </ul>
           </div>
+
       </section>
+        <button
+            onClick={() => alert('Invite')}
+            className="inline-flex bg-myrtleGreen items-center px-2.5 py-1.5 border border-transparent text-xs font-medium rounded shadow-sm text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+            Invite
+        </button>
+
+        <h2 className="text-2xl font-bold leading-7 text-gray-900 sm:text-3xl sm:truncate" id="section-1-title">
+            Contacts
+        </h2>
+        <section aria-labelledby="section-1-title">
+            <div className="flow-root mt-6">
+                <ul className="-my-5 divide-y divide-gray-200">
+                    {directChannels.map((ch) =>
+                        <li className="py-4" key={ch.channel.address.toBase58()}>
+                            <div className="flex items-center space-x-4">
+                                <div className="flex-1 min-w-0">
+                                    <p className="text-sm font-medium text-gray-900 truncate">
+                                        {ch.contact.alias}
+                                    </p>
+                                </div>
+                                <div>
+                                    {channel?.address.toBase58() !== ch.channel.address.toBase58() && <button
+                                      onClick={() => setCurrentChannel(ch.channel)}
+                                      className="inline-flex bg-myrtleGreen items-center px-2.5 py-1.5 border border-transparent text-xs font-medium rounded shadow-sm text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+                                      Chat
+                                    </button>}
+
+                                </div>
+                            </div>
+                        </li>
+                    )}
+                </ul>
+            </div>
+        </section>
+        <button
+            onClick={() => alert('Add Contact')}
+            className="inline-flex bg-myrtleGreen items-center px-2.5 py-1.5 border border-transparent text-xs font-medium rounded shadow-sm text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+            Add Contact
+        </button>
     </div>
 
   );

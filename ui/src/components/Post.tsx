654ef52b505f1b6b889c796bdd10045b355a1a4e
Martin Riedel <web@riedel-privat.de>
2021-06-05 00:26:00 -0400

    feat: styled post component, added message size limit of 120

--- ui/src/components/Post.tsx
+++ ui/src/components/Post.tsx
@@ -5,29 +5,39 @@ type Props = {
   disabled: boolean
 }
 
+const MESSAGE_LIMIT = 120
+
 const Post = ({post, disabled}: Props) => {
   const [message, setMessage] = useState<string>();
   return (
     <div className="flex space-x-3">
       <div className="flex-1 space-y-1">
-        <div className="flex items-center justify-between">
-          <form className="w-full" onSubmit={(event) => {
-            event.preventDefault()
-            message && post(message)
-          }}>
-            <label htmlFor="message" className="sr-only">
-              Message
-            </label>
-            <input
-              type="text"
-              name="message"
-              id="message"
-              className="text-myrtleGreen disabled:opacity-50 shadow-sm focus:ring-aeroBlue-500 focus:border-aeroBlue-500 block w-full sm:text-sm border-gray-300 rounded-md"
-              placeholder="message"
-              onChange={event => setMessage(event.target.value)}
-              disabled={disabled}
-            />
-          </form>
+        <div className="flex items-center justify-between space-x-4">
+          <div className="flex-1">
+            <form className="w-full" onSubmit={(event) => {
+              event.preventDefault()
+              message && post(message)
+            }}>
+              <label htmlFor="message" className="sr-only">
+                Message
+              </label>
+              <input
+                type="text"
+                name="message"
+                id="message"
+                className="bg-myrtleGreen border-0 rounded w-full py-2 px-4 text-aeroBlue-light placeholder-aeroBlue-dark leading-tight focus:outline-none focus:border-aeroBlue"
+                maxLength={MESSAGE_LIMIT}
+                placeholder="message"
+                onChange={event => setMessage(event.target.value)}
+                disabled={disabled}
+              />
+            </form>
+          </div>
+        </div>
+        <div className="flex items-center flex-row-reverse">
+          <div className="">
+            <p>{'' + (message ? message.length : '0') + '/' + MESSAGE_LIMIT }</p>
+          </div>
         </div>
       </div>
     </div>

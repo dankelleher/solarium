dc5bfc00eff755c99dff5615f447b3a60f644a0f
dankelleher <dankelleher@yahoo.com>
2021-06-22 19:37:09 +0200

    a client-side http -> https redirect as cloudflare does not support it for ipfs-hosted sites

--- ui/src/index.tsx
+++ ui/src/index.tsx
@@ -4,6 +4,10 @@ import './styles/index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+if (window.location.protocol !== 'https:' && window.location.hostname !== 'localhost') {
+  window.location.replace(`https:${window.location.href.substring(window.location.protocol.length)}`);
+}
+
 ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change

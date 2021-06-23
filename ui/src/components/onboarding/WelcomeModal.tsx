6f0616d5a9e40457fec9bc6295325e6296084d81
Martin Riedel <web@riedel-privat.de>
2021-06-23 07:42:34 -0400

    feat: added content for Welcome Modal

--- ui/src/components/onboarding/WelcomeModal.tsx
+++ ui/src/components/onboarding/WelcomeModal.tsx
@@ -19,7 +19,7 @@ const WelcomeModal: React.FC<Props> = ({
       <Dialog
         as="div"
         static
-        className="fixed z-10 inset-0 overflow-y-auto"
+        className="fixed z-20 inset-0 overflow-y-auto"
         open={show}
         onClose={() => {}}
       >
@@ -51,21 +51,48 @@ const WelcomeModal: React.FC<Props> = ({
             leaveFrom="opacity-100 translate-y-0 sm:scale-100"
             leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
           >
-            <div className="inline-block align-bottom bg-white rounded-lg px-4 pt-5 pb-4 text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full sm:p-6">
+            <div className="inline-block align-bottom bg-white rounded-lg px-4 pt-5 pb-4 text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-3xl sm:w-full sm:p-6">
               <div>
                 <div className="mx-auto flex items-center justify-center h-12 rounded-full bg-myrtleGreen">
                   <Logo/>
                 </div>
                 <div className="mt-3 text-center sm:mt-5 text-myrtleGreen">
                   <Dialog.Title as="h3" className="text-lg leading-6 font-medium">
-                    Welcome Screen
+                    Welcome to SOLARIUM
                   </Dialog.Title>
-                  <Dialog.Description>
-                    Let's get you set up...
-                  </Dialog.Description>
-                  <div className="mt-2 py-6">
-                    <h1>Let's put some content in here!</h1>
+
+                  <div className="p-6 text-left">
+                    <div className="flow-root mt-1">
+                      <p className="mt-1 line-clamp-2">
+                        Solarium is a fully decentralised, end-to-end encrypted, censorship-resistant instant messenger based on the <a className="font-bold" href="https://solana.com" rel="nofollow" target="_blank">Solana</a> blockchain.
+                      </p>
+                      <p className="mt-1 line-clamp-2">
+                        Visit our <a className="font-bold" href="https://github.com/dankelleher/solarium" rel="nofollow" target="_blank">Github Page</a> to find out more about the project!
+                      </p>
+                    </div>
+                    <div className="flow-root mt-3">
+                      <h3 className="text-lg leading-6 font-medium">
+                        A Solarium primer video
+                      </h3>
+                      <div className="aspect-w-16 aspect-h-9">
+                        <iframe src="https://www.youtube.com/embed/C_XVl1fT76Y" frameBorder="0"
+                                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                                allowFullScreen></iframe>
+                      </div>
+                    </div>
+
+                    <div className="flow-root mt-3">
+                      <h3 className="text-lg leading-6 font-medium">
+                        The technology behind solarium
+                      </h3>
+                      <div className="aspect-w-16 aspect-h-9">
+                        <iframe src="https://www.youtube.com/embed/kgDRefJECIE" frameBorder="0"
+                                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                                allowFullScreen></iframe>
+                      </div>
+                    </div>
                   </div>
+
                 </div>
               </div>
               <div className="mt-5 sm:mt-6 text-center">
